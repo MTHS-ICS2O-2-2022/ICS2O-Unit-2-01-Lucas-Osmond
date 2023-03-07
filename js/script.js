b7bf5763cd9c6f8b4e8b4ee4dbad0bbcb8f378d1
Lucas-Osmond <124389349+Lucas-Osmond@users.noreply.github.com>
2023-03-07 18:05:52 +0000

    added js hello world

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Lucas Osmond All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Lucas Osmond
+// Created on: Feb 2023
 // This file contains the JS functions for index.html
 
 
 function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "Hello, World!"
+  document.getElementById(hello-world).innerHTML = "Hello, World!";
 }
